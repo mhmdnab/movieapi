51540930d5d926007fa5a2d0cdd4a4c0ff1634b7
mhmdnab <mhmdnab004@gmail.com>
2025-06-12 03:26:34 +0300

    finished version 1

--- src/app/(app)/genres/[id]/page.tsx
+++ src/app/(app)/genres/[id]/page.tsx
@@ -1,27 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
-interface Params {
+export default async function GenrePage({
+  params,
+}: {
   params: { id: string };
-}
-
-export default async function GenrePage({ params }: Params) {
+}) {
   const genreId = params.id;
   const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
-  // Fetch movies for this genre
   const moviesRes = await fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`
   );
   const moviesData = await moviesRes.json();
-  const movies = moviesData.results;
 
-  // Fetch all genres and find this one
   const genreRes = await fetch(
     `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`
   );
   const genreData = await genreRes.json();
+
   const genre = genreData.genres.find((g: any) => g.id.toString() === genreId);
 
   return (
@@ -31,7 +28,7 @@ export default async function GenrePage({ params }: Params) {
       </h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-        {movies.map((movie: any) => (
+        {moviesData.results.map((movie: any) => (
           <Link
             href={`/movies/${movie.id}`}
             key={movie.id}
@@ -41,8 +38,8 @@ export default async function GenrePage({ params }: Params) {
               <Image
                 src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
                 alt={movie.title}
-                layout="fill"
-                objectFit="cover"
+                fill
+                className="object-cover"
               />
             </div>
             <div className="p-3">
